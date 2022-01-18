beb1cdd18b604ee015dfe1036afbbbc644b4c7c1
Tobias Eriksson <toberi@outlook.com>
2022-01-18 22:57:18 +0100

    refactoring

--- page.js
+++ page.js
@@ -1,3 +1,22 @@
+const nav = document.querySelector(".primary-nav");
+const toggle = document.querySelector(".nav-toggle");
+
+toggle.addEventListener("click", () => {
+  var visible = nav.getAttribute("data-visible");
+  visible === "false"
+    ? nav.setAttribute("data-visible", "true") &
+      toggle.setAttribute("aria-expanded", "true")
+    : nav.setAttribute("data-visible", "false") &
+      toggle.setAttribute("aria-expanded", "false");
+});
+
+window.addEventListener("resize", () => {
+  nav.style.transition = "none";
+  setTimeout(() => {
+    nav.style.transition = "transform 250ms ease";
+  }, 100);
+});
+
 var slideIndex = 1;
 
 showSlideSpw1(slideIndex);
